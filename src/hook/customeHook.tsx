74bfa2847867c85ef9cd2152dfa851dad9854024
Adityaraj002 <adityaraj99106@gmail.com>
2025-07-24 05:23:41 +0530

    custome hook added

---
+++ src/hook/customeHook.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const customeHook = () => {
+  return (
+    <View>
+      <Text>THis is hook</Text>
+    </View>
+  )
+}
+
+export default customeHook
+
+const styles = StyleSheet.create({})
